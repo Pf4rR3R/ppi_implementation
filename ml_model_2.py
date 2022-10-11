0dc75eae4b939356c746a3002e2c933ba8b06ea4
Marie Mink <marie@MacBook-Air-von-Marie.local>
2022-10-11 16:55:21 +0200

    clean file

--- ml_model_2.py
+++ ml_model_2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[36]:
-
-
 import pandas as pd
 import numpy as np
 import requests
@@ -14,77 +11,37 @@ from sklearn.metrics import (
     confusion_matrix,
 )
 
-
-# In[6]:
-
-
 url = "http://localhost:8000/n_chunks"
 response = requests.get(url)
 response.json()["chunks"]
-
-
-# In[17]:
-
 
 url = "http://localhost:8000/chunk/1"
 response1 = requests.get(url)
 json_text1 = response1.text
 
-
-# In[18]:
-
-
 short1 = json_text1[1:-1]
-short1 = short1.replace("\\","")
-
-
-# In[19]:
-
+short1 = short1.replace("\\", "")
 
 url = "http://localhost:8000/chunk/2"
 response2 = requests.get(url)
 json_text2 = response2.text
 
-
-# In[20]:
-
-
 short2 = json_text2[1:-1]
-short2 = short2.replace("\\","")
-
-
-# In[21]:
+short2 = short2.replace("\\", "")
 
 
 df1 = pd.read_json(short1, orient="records")
-df2 = pd.read_json(short2,orient="records")
+df2 = pd.read_json(short2, orient="records")
 
-
-# In[32]:
-
-
-data = pd.concat([df1,df2]).dropna()
-
-
-# In[33]:
-
+data = pd.concat([df1, df2]).dropna()
 
 X_train, X_test, y_train, y_test = train_test_split(
-        data[["gender"]],
-        data["Retention"],
-    )
-
-
-# In[34]:
-
+    data[["gender"]], data["Retention"],
+)
 
 # Randomforest
 clf = RandomForestClassifier(max_depth=2, random_state=0)
 y_pred = clf.fit(X_train, y_train).predict(X_test)
-
-
-# In[41]:
-
 
 cm = confusion_matrix(y_test, y_pred)
 # sns.heatmap(cm, square=True, annot=True, fmt='d', cmap="Blues")
@@ -93,10 +50,3 @@ cm = confusion_matrix(y_test, y_pred)
 
 print("random forest classification report:")
 print(classification_report(y_test, y_pred))
-
-
-# In[ ]:
-
-
-
-
